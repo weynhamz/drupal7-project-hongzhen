7aaffdb62a4199190ad030ec57f93a37dbfb6468
Techlive Zheng <techlivezheng@gmail.com>
2011-05-28 22:56:37 +0800

    Theme color changed from blue to red.

--- sites/all/themes/hongzhen/templates/views-view--frontpage.tpl.php
+++ sites/all/themes/hongzhen/templates/views-view--frontpage.tpl.php
@@ -48,13 +48,11 @@
       <?php print $exposed; ?>
     </div>
   <?php endif; ?>
-<!--
   <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="626" height="454">
     <param name="movie" value="sites/all/themes/hongzhen/images/home.swf" />
     <param name="quality" value="high" />
     <embed src="sites/all/themes/hongzhen/images/home.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="626" height="454"></embed>
   </object>
--->
   <?php if ($attachment_before): ?>
     <div class="attachment attachment-before">
       <?php print $attachment_before; ?>
